1cbeac21b4a49aa16160a94aefd8f0b7dca73395
Rcurvo <renan.curvo@gmail.com>
2024-02-27 19:22:43 -0300

    feat: add fulfilment webhook

--- server.js
+++ server.js
@@ -4,12 +4,33 @@ import { postChat } from './routes/post-chat.js';
 import { getCustumers } from './routes/get-customers.js';
 import { CustomerRepository } from './db/customer-repository.js';
 import { ProductsRepository } from './db/products-repository.js';
+import fulfilment from "dialogflow-fulfillment"
 
 
 const app = express()
 const port = process.env.PORT || 3030;
 export const customerRepository = new CustomerRepository()
 export const productRepository = new ProductsRepository()
+
+app.get("/", (req, res) =>{
+    res.send("server running")
+})
+
+app.post("/webhook", express.json(), (req, res)=>{
+    const agent = new fulfilment.WebhookClient({
+        request: req,
+        response: res
+    })
+
+    function discountResponse(agent){
+        agent.add(`Esses são nossos produtos em promoção ${JSON.stringify(productRepository.list())}`)
+    }
+
+    const intentMap = new Map();
+    intentMap.set("Promoção", discountResponse)
+
+    agent.handleRequest(intentMap)
+})
 
 app.use("/products", getProducts)
 
